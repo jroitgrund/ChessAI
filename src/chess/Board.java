8a007de687107691c4fd1a42304f75b4a8691790
Jonathan Roitgrund <jonathan.roitgrund10@imperial.ac.uk>
2012-10-18 13:04:25 +0100

    Format

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -88,6 +88,7 @@ public class Board {
   void clearPiece(Coord c) {
     pieces[c.getCol()][c.getRow()] = null;
   }
+
   void display() {
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
