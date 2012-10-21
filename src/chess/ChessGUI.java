31180c2733253f00d9c7f772fde93805eadbaa36
Jonathan Roitgrund <jonathan.roitgrund10@imperial.ac.uk>
2012-10-21 18:16:55 +0100

    Modularized UI, easy to set game type.

--- src/chess/ChessGUI.java
+++ src/chess/ChessGUI.java
@@ -13,6 +13,7 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 public class ChessGUI {
 
@@ -40,79 +41,113 @@ public class ChessGUI {
 }
 
 enum GameState implements State {
+  WAITING {
+
+    @Override
+    public GameState mouseReleased(ChessPanel cp, MouseEvent e) {
+      return WAITING;
+    }
+
+    @Override
+    public GameState mousePressed(ChessPanel cp, MouseEvent e) {
+      return WAITING;
+    }
+
+    @Override
+    protected GameState continueGame(ChessPanel cp) {
+      return null;
+    }
+    
+  },
+  PLAYINGAI {
+
+    Coord from = null;
+
+    @Override
+    public GameState mouseReleased(ChessPanel cp, MouseEvent e) {
+      Coord b = from;
+      Coord c = null;
+      from = null;
+      if (b == null || (c = getCoord(e)) == null) {
+        return this;
+      }
+      return move(cp, b, c);
+    }
+
+    @Override
+    public GameState mousePressed(ChessPanel cp, MouseEvent e) {
+      from = getCoord(e);
+      return this;
+    }
+
+    @Override
+    protected GameState continueGame(final ChessPanel cp) {
+      System.out.println("Creating new SwingWorker for w");
+      new SwingWorker<GameState, Void>() {
+
+        @Override
+        protected GameState doInBackground() throws Exception {
+          System.out.println("worker starting");
+          Move bestMove = Minimax.bestMove(cp.b);
+          cp.b.move(bestMove.getFrom(), bestMove.getTo(), false);
+          System.out.println("worker done moving");
+          switch (cp.b.getState()) {
+            case ONGOING:
+              System.out.println("worker returning PLAYINGAI");
+              return PLAYINGAI;
+            case CHECKMATE:
+              return VICTORY;
+            case DRAW:
+              return STALE;
+          }
+          return PLAYINGAI;
+        }
+        
+        protected void done()
+        {
+          try {
+            cp.setState(get());
+            cp.repaint();
+          }
+          catch (Exception e) {
+          }
+        }
+
+      }.execute();
+      return WAITING;
+    }
+  },
   PLAYING {
 
     Coord from = null;
 
     @Override
-    public void paint(ChessPanel cp, Graphics g) {
-      g.drawImage(cp.bg, 0, 0, null);
-      for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-          Piece p = cp.b.getPiece(new Coord(i, j));
-          if (p != null) {
-            g.drawImage(
-                cp.pieces[p.getColor().ordinal()][p.getType().ordinal()],
-                8 + i * 28, 240 - 36 - j * 28, null);
-          }
-        }
-      }
-    }
-
-    @Override
     public GameState mouseReleased(ChessPanel cp, MouseEvent e) {
       Coord b = from;
-      Coord c = getCoord(e);
+      Coord c = null;
       from = null;
-      if (b == null || !c.inBoard()) {
+      if (b == null || (c = getCoord(e)) == null) {
         return this;
       }
-      if (cp.b.move(b, c, true)) {
-        switch (cp.b.getState()) {
-          case ONGOING:
-            Move bestMove = Minimax.bestMove(cp.b);
-            cp.b.move(bestMove.getFrom(), bestMove.getTo(), false);
-            switch (cp.b.getState()) {
-              case ONGOING:
-                return this;
-              case CHECKMATE:
-                return VICTORY;
-              case DRAW:
-                return STALE;
-            }
-            return this;
-          case CHECKMATE:
-            return VICTORY;
-          case DRAW:
-            return STALE;
-        }
-      }
-      return this;
+      return move(cp, b, c);
     }
 
     @Override
     public GameState mousePressed(ChessPanel cp, MouseEvent e) {
-      Coord c = getCoord(e);
-      if (!c.inBoard()) {
-        from = null;
-      }
-      else {
-        from = c;
-      }
+      from = getCoord(e);
       return this;
     }
 
-    private Coord getCoord(MouseEvent e) {
-      int x = e.getX() - 8;
-      int y = 240 - e.getY() - 8;
-      return new Coord(x / 28, y / 28);
+    @Override
+    protected GameState continueGame(ChessPanel cp) {
+      return this;
     }
   },
   VICTORY {
 
     @Override
     public void paint(ChessPanel cp, Graphics g) {
-      PLAYING.paint(cp, g);
+      super.paint(cp, g);
       g.setColor(Color.BLACK);
       StringBuilder s = new StringBuilder(11);
       switch (cp.b.getCurrentPlayer()) {
@@ -138,12 +173,17 @@ enum GameState implements State {
     public GameState mousePressed(ChessPanel cp, MouseEvent e) {
       return this;
     }
+
+    @Override
+    protected GameState continueGame(ChessPanel cp) {
+      return null;
+    }
   },
   STALE {
 
     @Override
     public void paint(ChessPanel cp, Graphics g) {
-      PLAYING.paint(cp, g);
+      super.paint(cp, g);
       g.setColor(Color.BLACK);
       g.drawString("Stalemate", 120, 120);
       g.drawString("Click to restart", 120, 160);
@@ -159,7 +199,49 @@ enum GameState implements State {
     public GameState mousePressed(ChessPanel cp, MouseEvent e) {
       return this;
     }
+
+    @Override
+    protected GameState continueGame(ChessPanel cp) {
+      return null;
+    }
   };
+
+  protected Coord getCoord(MouseEvent e) {
+    int x = e.getX() - 8;
+    int y = 240 - e.getY() - 8;
+    Coord c = new Coord(x / 28, y / 28);
+    return c.inBoard() ? c : null;
+  }
+
+  public void paint(ChessPanel cp, Graphics g) {
+    g.drawImage(cp.bg, 0, 0, null);
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+        Piece p = cp.b.getPiece(new Coord(i, j));
+        if (p != null) {
+          g.drawImage(cp.pieces[p.getColor().ordinal()][p.getType().ordinal()],
+              8 + i * 28, 240 - 36 - j * 28, null);
+        }
+      }
+    }
+  }
+
+  protected GameState move(ChessPanel cp, Coord from, Coord to) {
+    if (cp.b.move(from, to, true)) {
+      cp.repaint();
+      switch (cp.b.getState()) {
+        case ONGOING:
+          return continueGame(cp);
+        case CHECKMATE:
+          return VICTORY;
+        case DRAW:
+          return STALE;
+      }
+    }
+    return this;
+  }
+
+  abstract protected GameState continueGame(ChessPanel cp);
 }
 
 interface State {
@@ -181,9 +263,9 @@ class ChessPanel extends JPanel implements MouseListener {
   Board             b;
 
   private GameState state;
-
+  
   public ChessPanel() {
-    state = GameState.PLAYING;
+    state = GameState.PLAYINGAI;
     pieces = new BufferedImage[2][6];
     try {
       pieces[0][0] = ImageIO.read(new File("img/whites/white_p.png"));
@@ -204,6 +286,11 @@ class ChessPanel extends JPanel implements MouseListener {
     }
     addMouseListener(this);
     b = new Board();
+  }
+  
+  public void setState(GameState gameState) {
+    state = gameState;
+    
   }
 
   public Dimension getPreferredSize() {
